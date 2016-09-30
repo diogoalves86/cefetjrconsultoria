72afd3e20fe02854b10c8a54aee0e13064c1f451
Diogo Alves <diogoalvessilva86@gmail.com>
2016-09-30 16:05:08 -0300

    Aplicação inicial

---
+++ Cefet JR Consultoria/Cefet JR Consultoria/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cefet_JR_Consultoria
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTeste_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            Janela2 janela = new Janela2();
+            janela.Show();
+
+        }
+    }
+}
